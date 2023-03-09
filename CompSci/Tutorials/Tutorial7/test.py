a74abbd684c6364061f72be6350d4fd7abe01e9f
HeldEnderMen <ajantefraser13@gmail.com>
2023-03-09 09:01:19 -0400

    ye

--- CompSci/Tutorials/Tutorial7/test.py
+++
@@ -1,5 +0,0 @@
-height = float(input("Enter Height in meters: "))
-foot = height / 0.3048
-foot_whole = int(foot)
-inches = int((foot - foot_whole) * 12)
-print(f"{height:.2f} meters is equal to {foot_whole} feet and {inches} inches.")
